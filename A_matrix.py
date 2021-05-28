c908eb8b5bab6da04b771c23ed3ef62b9c127f42
Jana <jadracka@gmail.com>
2021-05-28 14:40:59 +0200

    .

--- A_matrix.py
+++ A_matrix.py
@@ -17,12 +17,7 @@ import MainCode as MC
 #from operator import itemgetter
 #from collections import namedtuple
 
-# Are there two epochs to calculate?
-if len(cg.Which_epochs)>1:
-    Two_epochs = True
-else:
-    Two_epochs = False
-    
+   
 Nominal_coords = MC.Nominal_coords   
 LoS_measurements = MC.LoS_measurements
 Pol_measurements = MC.Pol_measurements
@@ -31,7 +26,7 @@ StDevs_IFM_measurements = MC.StDevs_IFM_measurements
 measured_distances_in_lines = MC.measured_distances_in_lines
 sorted_measured_points_in_lines = MC.sorted_measured_points_in_lines
 
-if Two_epochs:
+if MC.Two_epochs:
     LoS_measurements_E1 = MC.LoS_measurements_E1
     Pol_measurements_E1 = MC.Pol_measurements_E1
     Pol_measurements_cart_E1 = MC.Pol_measurements_cart_E1
@@ -39,5 +34,3 @@ if Two_epochs:
     measured_distances_in_lines_E1 = MC.measured_distances_in_lines_E1
     sorted_measured_points_in_lines_E1 = MC.sorted_measured_points_in_lines_E1
 
-
-    
